8ca72ff7cc3d15dd2aabb9057fdf71135acb5856
linyaqi <mr03web@foxmail.com>
2018-08-21 16:23:00 +0800

    调整

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,10 @@
 declare module "react-native-set-soft-input-mode" {
   const SoftInputMode: {
-    set: (value: any) => void
-  }
+    set: (value: any) => void;
+    ADJUST_NOTHING: any;
+    ADJUST_PAN: any;
+    ADJUST_RESIZE: any;
+    ADJUST_UNSPECIFIED: any;
+  };
   export default SoftInputMode;
 }
